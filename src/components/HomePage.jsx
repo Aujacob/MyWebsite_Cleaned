e48afa06b2caed5fc58a29dc9fd08a6ec684bc0f
Augustine Jacob <71908659+Aujacob@users.noreply.github.com>
2026-05-29 00:05:09 -0400

    Update homepage bio
    Hobbies + career path changed, so now homepage bio must be updated

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -33,7 +33,7 @@ const HomePage = () => {
           />
           <div className="name">Augustine Jacob</div>
          
-          <div className="role"> <br/>Software Engineer / Artist</div>
+          <div className="role"> <br/>Skier / Artist</div>
 
           <div className="location"> Metro Detroit</div>
 
@@ -48,13 +48,12 @@ const HomePage = () => {
           <div className="about-title">A Bit About Me</div>
           <div className="about-desc">
               
-            Wayne State University alumni with a B.E. in Computer Science. <br /> <br />
-            In my free time, I enjoy making things with my two hands. <br />
-            Whether that's painting, photography, software projects, or growing jalapenos. <br /><br />
-            I've always felt strongly in "Use it or Lose it". <br />Software engineering is a lifelong pursuit of knowledge. <br />
-            When you stop learning, you start falling behind. <br />
-            I am currently studying for the Comptia Network+ certification,<br /> developing this website, or learning game development.<br />
-            I'm always open to discussing my projects or an opportunity to work with you.<br /><br />
+            Wayne State University graduate with a B.E. in Computer Science, <br />
+            but now pivoted to sales for a software company.   <br />
+            My technical background gives me a genuine edge in understanding what customers need and how software actually solves their problems. <br /><br />
+            In my free time, I enjoy making things with my two hands. <br></br>
+            Whether that's painting, restoring old game consoles, software projects, growing jalapenos, or developing this website.<br />
+            I'm always open to discussing my projects with you.<br /><br />
             Feel free to reach out!
             
           </div>
